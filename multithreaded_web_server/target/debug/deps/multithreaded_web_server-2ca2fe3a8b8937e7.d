6eebcdb362b19b77f932532ab094b10618308114
w-a-gomes <wildenalisson@outlook.com>
2022-01-02 01:30:43 -0300

    Web server

---
+++ multithreaded_web_server/target/debug/deps/multithreaded_web_server-2ca2fe3a8b8937e7.d
@@ -0,0 +1,5 @@
+/home/alien/Scripts/Git/GitHub/rust_studies/multithreaded_web_server/target/debug/deps/multithreaded_web_server-2ca2fe3a8b8937e7.rmeta: src/main.rs
+
+/home/alien/Scripts/Git/GitHub/rust_studies/multithreaded_web_server/target/debug/deps/multithreaded_web_server-2ca2fe3a8b8937e7.d: src/main.rs
+
+src/main.rs:
